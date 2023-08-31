d35800efe0a24b9c1a3448559dfa8f1d95e22b7c
Kevin Luckhai <116979866+KevLuck@users.noreply.github.com>
2023-08-31 12:11:15 -0400

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,44 @@
-const Category = require('./models/Category');
-
-class Product {
-  constructor(name, price, stock, categoryId, tagIds) {
-    this.name = name;
-    this.price = price;
-    this.stock = stock;
-    this.categoryId = new Category(categoryId);
-    this.tagIds = tagIds;
-  }
-}
-
-module.exports = Product;
+// models/Product.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Product extends Model {}
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true, The following code creates a connection to the `ecommerce_db` database and defines the `Category`, `Product`, `ProductTag`, and `Tag` models:
