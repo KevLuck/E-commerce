e22f6e4635337a057d732b577f7938c14ab06689
Kevin Luckhai <116979866+KevLuck@users.noreply.github.com>
2023-08-31 12:09:03 -0400

    Update ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,36 +1,36 @@
+// models/ProductTag.js
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
-
 class ProductTag extends Model {}
-
 ProductTag.init(
   {
-    // define columns
-    productId: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: {
-        model: Product,
-        key: 'id',
-      },
+      primaryKey: true,
+      autoIncrement: true
     },
-    tagId: {
+    product_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
-        model: Tag,
-        key: 'id',
-      },
+        model: 'product',
+        key: 'id'
+      }
     },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'product_tag'
   }
 );
-
 module.exports = ProductTag;
