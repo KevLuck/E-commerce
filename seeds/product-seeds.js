a5380bb89ef0e149f460236cf7b000475a8b0444
Kevin Luckhai <116979866+KevLuck@users.noreply.github.com>
2023-08-31 12:12:59 -0400

    Update product-seeds.js

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -1,38 +1,60 @@
+// seeds/product-seeds.js
 const { Product } = require('../models');
-
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
-    price: 14.99,
-    stock: 14,
-    category_id: 1,
+    product_name: 'Apple iPhone 12',
+    price: 999.99,
+    stock: 10,
+    category_id: 1
   },
   {
-    product_name: 'Running Sneakers',
-    price: 90.0,
-    stock: 25,
-    category_id: 5,
+    product_name: 'Samsung Galaxy S21',
+    price: 849.99,
+    stock: 15,
+    category_id: 1
   },
   {
-    product_name: 'Branded Baseball Hat',
-    price: 22.99,
-    stock: 12,
-    category_id: 4,
+    product_name: 'Google Pixel 5',
+    price: 699.99,
+    stock: 20,
+    category_id: 1
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
-    price: 12.99,
-    stock: 50,
-    category_id: 3,
+    product_name: 'Apple MacBook Air',
+    price: 999.99,
+    stock: 10,
+    category_id: 2
   },
   {
-    product_name: 'Cargo Shorts',
-    price: 29.99,
-    stock: 22,
-    category_id: 2,
+    product_name: 'Microsoft Surface Laptop 4',
+    price: 1299.99,
+    stock: 15,
+    category_id: 2
   },
+  {
+    product_name: 'Google Chromebook',
+    price: 299.99,
+    stock: 20,
+    category_id: 2
+  },
+  {
+    product_name: 'Sony PlayStation 5',
+    price: 499.99,
+    stock: 10,
+    category_id: 3
+  },
+  {
+    product_name: 'Microsoft Xbox Series X',
+    price: 499.99,
+    stock: 15,
+    category_id: 3
+  },
+  {
+    product_name: 'Nintendo Switch',
+    price: 299.99,
+    stock: 20,
+    category_id: 3
+  }
 ];
-
 const seedProducts = () => Product.bulkCreate(productData);
-
 module.exports = seedProducts;
