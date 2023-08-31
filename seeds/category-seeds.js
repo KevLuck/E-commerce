a92f372ad00f17e6523ba9c8329ef5915ed46d42
Kevin Luckhai <116979866+KevLuck@users.noreply.github.com>
2023-08-31 12:15:48 -0400

    Update category-seeds.js

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -1,5 +1,15 @@
+// seeds/category-seeds.js
 const { Category } = require('../models');
 const categoryData = [
   {
-    category_name: 'Smartphones'
+    category_name: 'Smartphones',
   },
+  {
+    category_name: 'Laptops',
+  },
+  {
+    category_name: 'Consoles',
+  },
+];
+const seedCategories = () => Category.bulkCreate(categoryData);
+module.exports = seedCategories;
