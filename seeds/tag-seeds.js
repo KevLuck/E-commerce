0d4b2d19aa86643f2217b48697986c7fbc2fd3a1
Kevin Luckhai <116979866+KevLuck@users.noreply.github.com>
2023-08-31 12:20:07 -0400

    Update tag-seeds.js

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,31 +2,15 @@ const { Tag } = require('../models');
 
 const tagData = [
   {
-    tag_name: 'rock music',
+    tag_name: 'electronics',
   },
   {
-    tag_name: 'pop music',
+    tag_name: 'phones',
   },
   {
-    tag_name: 'blue',
+    tag_name: 'laptops',
   },
-  {
-    tag_name: 'red',
-  },
-  {
-    tag_name: 'green',
-  },
-  {
-    tag_name: 'white',
-  },
-  {
-    tag_name: 'gold',
-  },
-  {
-    tag_name: 'pop culture',
-  },
-];
-
+  
 const seedTags = () => Tag.bulkCreate(tagData);
 
 module.exports = seedTags;
